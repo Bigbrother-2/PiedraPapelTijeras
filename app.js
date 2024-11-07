641d39b21499ae4982e5985aa218ea1718adf2f8
Flitz08 <marcosscotaro1315@gmail.com>
2024-11-06 21:15:51 -0300

    hasta el movimiento de arriba a abajo

--- app.js
+++ app.js
@@ -41,3 +41,46 @@ xInstrucciones.addEventListener("click",()=>{
 
 // Esta seccion es para cambiar el tema. Osea de oscuro a claro y viceversa. 
 
+// Esta seccion es de movimiento de la palma de la mano
+
+let palmaMano = document.querySelectorAll(".imagenMovimientoGrande")
+
+let disparadorMovimiento = document.querySelectorAll(".imagenMovimiento")
+
+let limiteSuperior = -50
+let limiteInferior = 50
+let direccion = 1
+let posicion = 0
+let intervalo
+let tiempo = 0
+disparadorMovimiento.forEach((imagen)=>{
+    imagen.addEventListener("click", iniciarMovimiento)
+})
+
+function iniciarMovimiento (){
+    // Si ya hay un intervalo corriendo, limpiarlo para evitar superposiciones
+    clearInterval(intervalo);
+
+    // Iniciar el intervalo
+    intervalo = setInterval(movimiento, 6); // Cada 20 ms se ejecuta la función `movimiento`
+
+    // Detener el movimiento después de 3 segundos
+    setTimeout(() => {
+        clearInterval(intervalo);
+    }, 1500); // 3000 ms = 3 segundos
+
+    
+}
+
+function movimiento(){
+        posicion += 2 * direccion // Esto cambia posicion en 2 pixeles
+        palmaMano.forEach((imagen)=>{
+            imagen.style.transform = `translateY(${posicion}px)`;
+            console.log(posicion)
+        }) 
+        if(posicion >=limiteInferior || posicion <= limiteSuperior){
+            direccion *= -1
+        }
+    
+}
+
