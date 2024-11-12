6bcc7bb748aa0ee18cdd4e53833bdb998110514c
Flitz08 <marcosscotaro1315@gmail.com>
2024-11-11 21:39:58 -0300

    agregue logica de comparacion

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ function movimiento(){
         posicion += 2 * direccion // Esto cambia posicion en 2 pixeles
         palmaMano.forEach((imagen)=>{
             imagen.style.transform = `translateY(${posicion}px)`;
-            console.log(posicion)
+            
         }) 
         if(posicion >=limiteInferior || posicion <= limiteSuperior){
             direccion *= -1
@@ -84,3 +84,53 @@ function movimiento(){
     
 }
 
+
+/* Esta parte decide la logica del juego */
+
+let eleccionJugador; //papel = 1 piedra = 2 tijera = 3
+let numeroRandomPC;
+
+
+//parte de contador de cuantas veces gano cada uno
+
+
+
+disparadorMovimiento.forEach((eleccion)=>{
+    eleccion.addEventListener("click", eleccionDelJugador)
+})
+
+function eleccionDelJugador(){
+    eleccionJugador = parseInt(event.target.getAttribute("data-value")) //Esto es para convertir el numero que con el getAttribute lo coloca como un string. Por eso tiene el parseInt para convertirlo en tipo number.
+    
+    eleccionPc()
+}
+
+function eleccionPc(){
+    numeroRandomPC = Math.floor(Math.random()*4)
+    comparacion()
+}
+
+function comparacion(){
+    if(eleccionJugador === 1 && numeroRandomPC === 2){
+        console.log("ganaste jugador")
+        console.log(eleccionJugador)
+        console.log(numeroRandomPC)
+    } else if(eleccionJugador === 2 && numeroRandomPC === 3){
+        console.log("ganaste jugador")
+        console.log(eleccionJugador)
+        console.log(numeroRandomPC)
+    } else if(eleccionJugador === 3 && numeroRandomPC === 1){
+        console.log("ganaste Jugador")
+        console.log(eleccionJugador)
+        console.log(numeroRandomPC)
+    } else if (eleccionJugador == numeroRandomPC){
+        console.log("Empataste")
+        console.log(eleccionJugador)
+        console.log(numeroRandomPC)
+    } else {
+        console.log("Perdiste")
+        console.log(eleccionJugador)
+        console.log(numeroRandomPC)
+}
+
+}
